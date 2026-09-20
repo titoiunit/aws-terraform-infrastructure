9fb99b1841888ca00b24b052e20ccc194b1b6d9a
Tritam <261806670+titoiunit@users.noreply.github.com>
2026-09-20 21:17:24 +0300

    feat: define dev module configuration

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -1,6 +1,10 @@
-# environments/dev/variables.tf
 variable "aws_region" {
-  description = "AWS region to deploy resources"
+  description = "AWS region in which to deploy resources"
   type        = string
   default     = "eu-west-1"
+}
+
+variable "bucket_name" {
+  description = "Globally unique name for the development S3 bucket"
+  type        = string
 }
